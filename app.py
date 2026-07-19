bb8e20d208e8da46580b3ba53345b9b71dba91a5
shilpa-jt12 <jtshilpa1208@gmail.com>
2026-07-19 22:52:18 +0530

    Add basic arithmetic operations in app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from calculator import add, subtract, multiply, divide
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 5))
